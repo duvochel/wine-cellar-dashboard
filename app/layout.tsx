15a014075d3213b1f0b13ea467915069a7d730f6
francois <franssois78@yahoo.fr>
2024-05-05 19:00:20 +0200

    feat: add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,18 @@
 import { inter } from '@/app/ui/fonts';
 
 import '@/app/ui/global.css';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Wine Cellar Dashboard',
+    default: 'Wine Cellar Dashboard',
+  },
+  description: 'The official Wine Cellar Dashboard.',
+  metadataBase: new URL(
+    'https://en.wikipedia.org/wiki/Pinard_(wine)#/media/File:Saint_Pinard.jpg',
+  ),
+};
 
 export default function RootLayout({
   children,
